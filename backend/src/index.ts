f5024ce41f496bec48418d6820ab1ed772d06f5f
Danilo de Jesus <danilo.de.jesus.dev@gmail.com>
2020-03-25 16:57:25 -0300

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import server from "./app";
 createConnection()
   .then(() => {
     server.listen(process.env.PORT || 3333, () => {
-      console.log("starting on 3333");
+      console.log(`starting on ${process.env.PORT || 3333} `);
     });
   })
   .catch(console.error);
