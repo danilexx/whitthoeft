cb02a5802382131194a188eb1a7f3dbfb5221053
Danilo de Jesus <danilo.de.jesus.dev@gmail.com>
2020-03-25 17:47:45 -0300

    env

--- frontend/src/services/index.jsx
+++ frontend/src/services/index.jsx
@@ -4,7 +4,6 @@ import Cookies from "js-cookie";
 const https = require("https");
 
 // const prod = process.env.NODE_ENV === "production";
-console.log(process.env.NEXT_PUBLIC_SERVER_URL, process.env);
 const baseURL = process.env.NEXT_PUBLIC_SERVER_URL;
 // "http://localhost:3333/";
 // ? "https://stormycommerce.com/api/"
